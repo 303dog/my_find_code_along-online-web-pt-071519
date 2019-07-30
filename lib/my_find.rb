9c69c789a92ff19ab845d428683070dedb32ca17
Paul Beckwith <303dog@gmail.com>
2019-07-30 03:04:13 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,9 @@ def my_find(collection)
   i = 0 
   #new_collection = []
   while i < collection.length 
-    if yield(collection[i]) return collection[i]
+    if yield(collection[i]) 
+      return collection[i]
     end
-      i += 1 
+    i += 1 
   end
 end
